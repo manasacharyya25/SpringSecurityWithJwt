d422af271b50c24f66b93786d8a3a5c566316647
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-07 08:12:00 +0530

    Changed default Form Based Authentication to Basic Authentication
    @Override
    configure(HttpSecurity http)
     {
          http
             .authorizeRequests()
             .anyRequest()
             .authenticated()
             .and()
             .httpBasic()
    }

--- src/main/java/com/somedman/SpringSecurityWithJwt/security/ApplicationSecurityConfig.java
+++ src/main/java/com/somedman/SpringSecurityWithJwt/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.somedman.SpringSecurityWithJwt.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,19 +11,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
 {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception
+  {
+    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+  }
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception
   {
-    auth.inMemoryAuthentication()
-        .withUser("foo").
-        password(passwordEncoder().encode("foo"))
-        .roles("USER");
+    auth.inMemoryAuthentication().withUser("foo").
+        password(passwordEncoder().encode("foo")).roles("USER");
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder()
+  {
     return new BCryptPasswordEncoder();
   }
 }
