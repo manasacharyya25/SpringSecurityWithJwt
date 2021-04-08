17246fc14affb1fec06cf3a65de859293d15d9e7
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-08 15:10:40 +0530

    userDetailsService as @Bean
    Creating userDetailsService as @Bean, no longer requires setting it up in AuthManagerBuilder.

--- src/main/java/com/somedman/SpringSecurityWithJwt/security/ApplicationSecurityConfig.java
+++ src/main/java/com/somedman/SpringSecurityWithJwt/security/ApplicationSecurityConfig.java
@@ -2,7 +2,6 @@ package com.somedman.SpringSecurityWithJwt.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,12 +23,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
   }
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception
-  {
-    auth.userDetailsService(userDetailsService());
-  }
-
-  @Override
+  @Bean
   protected UserDetailsService userDetailsService()
   {
     UserDetails foo = User.builder().username("foo").password(passwordEncoder().encode("foo")).roles("USER").build();
