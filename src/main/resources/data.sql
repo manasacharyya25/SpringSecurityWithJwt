7b9957b93bb0174aef0e6f25f7ae2ef4cd5454f9
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-18 13:56:29 +0530

    Added Sql Script to Initialize DB with Seed Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.application_user(
+	user_id, additional_information, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled, password, username)
+	VALUES (1, 'email.com', 'true', 'true', 'true', 'true', 'foo', 'foo');
+
+INSERT INTO public.application_user(
+	user_id, additional_information, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled, password, username)
+	VALUES (2, 'email.com', 'true', 'true', 'true', 'true', 'foo2', 'foo2');
